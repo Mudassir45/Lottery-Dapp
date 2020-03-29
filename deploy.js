1e257e94892de2f3d2e92a2f02958a6fd0e73f6b
Mudassir Ali <mudassirali36@gmail.com>
2020-03-30 00:48:44 +0500

    Getting Infura to Work

--- deploy.js
+++ deploy.js
@@ -3,7 +3,8 @@ const Web3 = require("web3");
 const { interface, bytecode } = require("./compile.js");
 
 const provider = new HDWalletProvider (
-    "bitter side biology dirt behave bean grain shrug drum obey nut digital"
+    "bitter side biology dirt behave bean grain shrug drum obey nut digital",
+    "https://ropsten.infura.io/v3/b2e9ef3a616c45f3b8b9a64ed3714848"
 );
 
 const web3 = new Web3(provider);
