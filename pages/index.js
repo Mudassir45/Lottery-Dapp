d74e6079ff43ee302114148081cba19123ce3335
Mudassir Ali <mudassirali36@gmail.com>
2020-04-02 20:13:24 +0500

    Getting balance of Contract

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,42 @@
 import React , {Component} from "react";
+import lottery from "../Lottery"; // to fetch the abi file
+import web3 from "../web3";
 
-class lottery extends Component {
+class Lottery extends Component {
+
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            manager: "",
+            participate_amount: "0.5",
+            message: "",
+            total_amount: ""
+    }
+}
+
+async componentDidMount() {
+    // get the Public address of the managers
+    const manager = await lottery.methods.manager().call();
+    console.log(manager);
+    this.setState({manager: manager});
+    const total_amount = await web3.eth.getBalance(Lottery.options.address) // or we can alse add directly address here
+    this.setState({total_amount: total_amount})
+}
+
     render() {
         return (
             <div>
-                <h1>Total Lottery Pool is 1000</h1>
+                <h1>Total Lottery Pool is {this.state.total_amount}</h1>
                 <form>
-                    <input value = "0.5"/>
+                    <input placeholder = "0.5"/>
                     <button type = "submit">Participate</button>
                 </form>
                 <hr/><br/><hr/>
-                <p>The Manager of the Lottery Dapp is </p>
+                <p>The Manager of the Lottery Dapp is {this.state.manager}</p>
                 <button>Pick Winner</button>
             </div>
         )
     }
 }
 
-export default lottery;
+export default Lottery;
